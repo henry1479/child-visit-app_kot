96f43f894b6f4d5db4f3bb7d0a991b910d7033dc
henry1479 <fokys.81@mail.ru>
2026-01-01 21:04:41 +0300

    add JsonParser

--- src/main/kotlin/org/henry1479/util/JsonParser.kt
+++ src/main/kotlin/org/henry1479/util/JsonParser.kt
@@ -1,9 +1,9 @@
 package org.henry1479.util
 
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import org.henry1479.children.Child
 import java.io.Reader
 import java.io.Writer
@@ -11,7 +11,7 @@ import java.io.Writer
 
 class JsonParser {
     companion object {
-        val parser: Gson = GsonBuilder().create();
+        val parser: Gson = GsonBuilder().create()
         fun <T> read(reader: Reader, resourceClass: Class<T>): T {
             return parser.fromJson<T>(reader, object : TypeToken<List<Child>>() {}.type)
         }
